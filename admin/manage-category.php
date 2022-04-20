ff899c2b8f7533a5068b38f318ca374b85d0d729
nhistory <nhistory@naver.com>
2022-04-19 23:54:17 -0300

    first commit

---
+++ admin/manage-category.php
@@ -0,0 +1,14 @@
+<?php include('partials/menu.php') ?>
+
+        <!-- Main Content Section Starts -->
+        <div class="main-content">
+            <div class="wrapper">
+                <h2>Manage Category</h2>
+
+
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <!-- Main Content Section Ends -->
+
+<?php include('partials/footer.php') ?>
